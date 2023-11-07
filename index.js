bdf9a321262c7dd757ba4513e5e4d60d961d6dc3
vibraniumSwaleh <hus.swaleh@yahoo.com>
2023-11-07 12:41:38 +0000

    setting up login page

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import axios from "axios";
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) =>{
-    res.send("Working!");
+    res.render("index.ejs", { content: "Working now!"});
 });
 
 app.listen(PORT, () => {
